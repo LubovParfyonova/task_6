9973dbfa35aab1fdfd52a848177cd2501b2587ca
Lubov <lubaparfyonova@gmail.com>
2022-01-04 06:27:02 +0200

    Add js code

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // fizz buzz
+
+let count = 0;
+const MAX = 100;
+
+while (count++ < MAX){
+    if (count % 3 === 0 && count % 5 === 0){
+        console.log('fizz buzz');
+    }else if (count % 3 === 0){
+        console.log('fizz');
+    }else if (count % 5 === 0){
+        console.log('buzz');
+    }else{
+        console.log(count);
+    }
+}
+console.log('Finish game');
